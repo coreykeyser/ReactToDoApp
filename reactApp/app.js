eb67babe0820efeb9cd4a7c59b4d3932c72c7a2b
coreykeyser <keyser.57@osu.edu>
2017-06-30 15:20:31 -0700

    to do list

--- reactApp/app.js
+++ reactApp/app.js
@@ -2,9 +2,68 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
+class ToDo extends React.Component {
+  // An individual todo item. The Todo boxes we've
+  // drawn in the picture in our head will include the task
+  // text as well as an X button to delete the Todo.
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <li>
+        <input
+          type="submit"
+          value="X"
+         />
+        {this.props.todo}
+      </li>
+    )
+  }
+}
 
+class ToDoList extends React.Component {
+  // The box around the assortment of Todos in our picture.
+  constructor(props){
+    super(props);
+    this.state = {
+      ToDos: ['do this','then that','then this','this then','that then']
+    }
+  }
+  render(){
+    return (
+      <ul>
+        {this.state.ToDos.map((x)=><ToDo todo={x} />)}
+      </ul>
+    )
+  }
+}
 
+// class InputLine extends React.Component {
+//   // A row at the top of our app that has an input field and a submit button.
+//   //  This is how a user will add new Todos.
+//   constructor(props){
+//     super(props)
+//   }
+//   render(){
+//     return (
+//
+//     )
+//   }
+// }
 
+// class ToDoApp extends React.Component {
+//   // A box around the whole thing. This is what we will pass into render,
+//   // and later we'll choose this as a good place put some functionality.
+//   constructor(props){
+//     super(props)
+//   }
+//   render(){
+//     return (
+//
+//     )
+//   }
+// }
 
-ReactDOM.render(<p>React lives!</p>,
+ReactDOM.render(<ToDoList />,
    document.getElementById('root'));
