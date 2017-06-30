941f39fbbaf8af52293dd1a55982e88c96031b8e
coreykeyser <keyser.57@osu.edu>
2017-06-30 15:37:28 -0700

    full static

--- reactApp/app.js
+++ reactApp/app.js
@@ -39,31 +39,45 @@ class ToDoList extends React.Component {
   }
 }
 
-// class InputLine extends React.Component {
-//   // A row at the top of our app that has an input field and a submit button.
-//   //  This is how a user will add new Todos.
-//   constructor(props){
-//     super(props)
-//   }
-//   render(){
-//     return (
-//
-//     )
-//   }
-// }
+class InputLine extends React.Component {
+  // A row at the top of our app that has an input field and a submit button.
+  //  This is how a user will add new Todos.
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="task"
+          onChange={(e) => this.handleMessage(e)}
+          value={this.state.value}
+         />
+        <input
+          type="submit"
+          value="Send"
+         />
+      </div>
+    )
+  }
+}
 
-// class ToDoApp extends React.Component {
-//   // A box around the whole thing. This is what we will pass into render,
-//   // and later we'll choose this as a good place put some functionality.
-//   constructor(props){
-//     super(props)
-//   }
-//   render(){
-//     return (
-//
-//     )
-//   }
-// }
+class ToDoApp extends React.Component {
+  // A box around the whole thing. This is what we will pass into render,
+  // and later we'll choose this as a good place put some functionality.
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>
+        <InputLine />
+        <ToDoList />
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<ToDoList />,
+ReactDOM.render(<ToDoApp />,
    document.getElementById('root'));
